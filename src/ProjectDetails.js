c35b69788e9c3e622061397fae69281d75e71eae
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-04-20 21:19:37 +0530

    update images in the portfolio

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -1,29 +1,60 @@
-import picture from "./picture/new1.jpg";
-import picture2 from "./picture/new2.jpg";
+import picture from "./picture/mern.jpg";
+import picture1 from "./picture/real state.jpg";
+import picture2 from "./picture/instagram.jpg";
+import picture3 from "./picture/pinterst.jpg";
+import picture4 from "./picture/portfolio.jpg";
+import picture5 from "./picture/food website.jpg";
+import picture6 from "./picture/note app.jpg";
+import picture7 from "./picture/frontend ecommerce.jpg";
+
 const projectDetails = [
     {
         img: picture,
-        heading: "Food Delivery Website",
-        info: "Developed a food delivery website using Html , Css, JavaScript and Bootstrap",
-        link: "http://foodfusionhub.avijithira.repl.co/",
+        heading: "Ecommerce-website",
+        info: "Developed a ecommerce-website using MERN stack technology, firebase, redux toolkit etc.",
+        link: "https://mern-ecommerce-website-mh4p.onrender.com/",
+    },
+    {
+        img: picture1,
+        heading: "Real State Market",
+        info: "Developed a Real state market website using MERN stack technology, firebase, redux toolkit etc.",
+        link: "https://mern-estate-market.onrender.com/",
     },
     {
         img: picture2,
+        heading: "Instagram Clone",
+        info: "Created an Instagram Clone website using Ejs, Tailwind Css, Node Js, Mongodb, Express Js",
+        link: "https://avijitinstagramclone.onrender.com/profile",
+    },
+    {
+        img: picture3,
+        heading: "Pinterest Clone",
+        info: "Developed a Pinter Clone website using Ejs, Tailwind Css, Node Js, Mongodb, Express Js",
+        link: "https://aviprinterestproject.onrender.com/",
+    },
+    {
+        img: picture4,
+        heading: "Portfolio Website",
+        info: "Develope a Portfolio Website using Html, Css and React Js",
+        link:"",
+    },
+    {
+        img: picture5,
+        heading: "Food Delivery Website",
+        info: "Developed a food delivery website using Html , Css, JavaScript and Bootstrap",
+        link: "https://avijit200318.github.io/Food-Website/",
+    },
+    {
+        img: picture6,
         heading: "Note App",
         info: "Devloped a Note app like google notes using React Js, Html and Css.",
         link: "https://avijit200318.github.io/Note-App/",
     },
     {
-        img: picture,
+        img: picture7,
         heading: "E-Commerce Website",
         info: "Developed a E-Commerce website using Html , Css, JavaScript.",
         link: "https://avijit200318.github.io/E-Commerce-Website/",
-    },
-    {
-        img: picture2,
-        heading: "Portfolio Website",
-        info: "Develope a Portfolio Website using Html, Css and React Js",
-        link:"",
     },
 ]
 
